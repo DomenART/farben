360517294e188dd5cd5117b77e3917c023f0dd10
Remzov <remzovnik@gmail.com>
2018-05-29 22:28:16 +0300

    form

--- src/js/autorization.js
+++ src/js/autorization.js
@@ -6,8 +6,9 @@ if (autorizationForm) {
     let passwordInput = document.querySelector('.js-password-input');
     let passwordLabel = document.querySelector('.js-password-label');
     let passwordToggler = document.querySelector('.js-password-toggler');
-    let passwordInputHidden = true
-
+    let submitBtn = document.querySelector('.autorization-form__submit');
+    let passwordInputHidden = true;
+ 
     emailInput.addEventListener('focus', emailFocusHandler);
     emailInput.addEventListener('focusout', emailFocusOutHandler);
     passwordInput.addEventListener('focus', passwordFocusHandler);
@@ -49,12 +50,21 @@ if (autorizationForm) {
         }
     }
 
-    autorizationForm.addEventListener('submit', autorizationFormSubmitHandler);
+    // submitBtn.addEventListener('click', autorizationFormSubmitHandler);
+    let inputs = document.querySelectorAll('input')
 
-    function autorizationFormSubmitHandler(event) {
-        event.preventDefault();
+    inputs.forEach(function(element){
+        element.addEventListener('keydown', autorizationFormSubmitKeyHandler);
+    })
+       
+
+    function autorizationFormSubmitKeyHandler(event) {
+        if (event.keyCode == 13) {
+            autorizationForm.classList.add('autorization-form_validated');
+        }
+
+        // event.preventDefault();
+        // autorizationForm.classList.add('autorization-form_validated');
     }
-
-
 
 }
